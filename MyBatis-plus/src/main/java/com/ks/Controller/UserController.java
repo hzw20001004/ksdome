ab9c19fe77b50320ce857d19d41d72cd19ced115
508578631@qq.com <hzw20011004>
2021-06-19 22:34:23 +0800

    6.19 晚上

--- MyBatis-plus/src/main/java/com/ks/Controller/UserController.java
+++ MyBatis-plus/src/main/java/com/ks/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.ks.Controller;
 import com.ks.Server.UserSrever;
 import com.ks.common.BaseController;
 import com.ks.common.BaseService;
+import com.ks.common.Dto;
 import com.ks.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +22,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/test")
 public class UserController extends BaseController<UserSrever, User> {
+    public Dto add(User user) {
+        return baseMapper.add(user);
+    }
 
+    public Dto update(User user) {
+        return baseMapper.update(user);
+    }
 
+    public Dto delete(List<Integer> ids) {
+        return baseMapper.delete(ids);
+    }
 
+    public Dto findByCondition() {
+        return baseMapper.findByCondition();
+    }
 
+    public Dto findById(Integer id) {
+        return baseMapper.findById(id);
+    }
 }
