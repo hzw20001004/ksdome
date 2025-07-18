b7e29b99bcb0db8d4bdc95766cb2c1412d4b6064
阿炜 <508578631@qq.com>
2025-07-18 16:01:11 +0800

    维护

--- Utils/集合转结构树/streamToTree.java
+++ Utils/集合转结构树/streamToTree.java
@@ -6,4 +6,15 @@ public List<SysXcxCityEntity> streamToTree(List<SysXcxCityEntity> treeList, Stri
 			.peek(child -> child.setChildren(streamToTree(treeList,child.getId().toString())))
 			.collect(Collectors.toList());
 }
-
+//带排序
+public List<SysXcxCityEntity> streamToTree(List<SysXcxCityEntity> treeList, String parentId) {
+	List<SysXcxCityEntity> sortedList = treeList.stream()
+			.filter(parent -> Objects.equals(parent.getParentId(), parentId))
+			.sorted(Comparator.comparing(SysXcxCityEntity::getSort, Comparator.nullsLast(String::compareTo)))
+			.collect(Collectors.toList());
+	// 递归处理子节点并设置到children字段
+	sortedList.forEach(node ->
+			node.setChildren(streamToTree(treeList, node.getId().toString()))
+	);
+	return sortedList;
+}
